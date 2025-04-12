2d04a2b1fba62f08bac6ac48bd54edf8c1701f1a
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-12 14:31:00 +0530

    Tailwind setup

--- FRONTEND/src/Components/Header.jsx
+++ FRONTEND/src/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='w-full h-[6vh] bg-[#212121] px-10 py-3 flex align-center justify-between text-white'>
+    <div className='fixed top-0 left-0 z-10 w-full h-[6vh] bg-[#212121] px-10 py-3 flex align-center justify-between text-white'>
     CodeLens
     <button className='bg-transparent border-[0.5px] border-gray-500 px-4 text-sm rounded-4xl'>PROFILE</button>
     </div>
--- FRONTEND/src/Components/Hero.jsx
+++ FRONTEND/src/Components/Hero.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Hero = () => {
+  const [code, setCode] = useState("");
+  function handelFileUplode(event) {
+    const file = event.target.files[0]
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setCode(e.target.result)
+      }
+      reader.readAsText(file)
+    }
+  }
   return (
-    <div className='flex flex-col items-center min-h-screen bg-[#212121] text-white px-4 relative'>
-      <div className='w-[50%] min-h-screen pb-32  '>
-      
+    <div className='flex flex-col items-center min-h-screen bg-[#212121] text-white px-4 relative '>
+      <div className='w-[50%] min-h-screen pb-70 text-xs whitespace-pre-wrap'>
+        
       </div>
-    
-      <div className='fixed bottom-0 w-[50%] px-4 h-[15vh] bg-[#212121] pb-5 flex gap-2'>
-        <input 
-          type='text' 
-          placeholder='Ask anything...' 
-          className=' w-full h-full bg-[#303030] text-sm text-white text-start px-5 py-2 rounded-xl outline-none  placeholder:text-gray-400 focus:ring-2 focus:ring-white-100 transition overflow-scroll'
-        />
 
-        <button className='cursor-pointer px-3 py-4 bg-transparent rounded-xl outline-none  placeholder:text-gray-400 border border-white-100 transition'>Ask</button>
-    
+      <div className='fixed bottom-1 w-[50%] px-4 h-[30vh] bg-[#303030] pb-5 flex flex-col justify-between  gap-4 rounded-2xl'>
+        <textarea
+          placeholder='Ask anything...'
+          value={code}
+          // onChange={(e) => setCode(e.target.value)}
+          className='w-full h-full bg-[#303030] text-xs text-white text-start px-5 py-2 rounded-xl outline-none placeholder:text-gray-400 transition overflow-x-hidden'
+          textarea/>
+        <div className='w-full flex items-center justify-between gap-2 bg-[#303030]'>
+          <input onChange={handelFileUplode}
+          type="file" 
+          accept='.js, .jsx, .ts, .tsx, .py, .java, .cpp, .c, .go, .rb, .php, .html, .css'
+          className='w-full bg-[#212121] p-2 rounded-xl text-sm border placeholder:text-gray-700 ' />
+          <button className=' w-full cursor-pointer bg-[#212121] p-2 rounded-xl text-sm outline-none  placeholder:text-gray-400 border border-white-100 transition'>Ask</button>
+        </div>
       </div>
+
     </div>
   )
 }
