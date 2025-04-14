9a47a50c3f9036935a34d8ba106e8f0b721eb52b
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 23:13:04 +0530

    frontEnd for LOGIN/SIGNUP Page

--- FRONTEND/src/Components/Header.jsx
+++ FRONTEND/src/Components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Header = () => {
 
@@ -12,6 +13,7 @@ const Header = () => {
     <div className='fixed top-0 left-0 z-10 w-full h-[10vh] bg-[#212121] px-10 flex items-center justify-between text-white text-lg'>
       <h1 className=''>CodeLens</h1>
       <button className='bg-transparent border-[0.5px] border-gray-500 px-6 text-lg rounded-xl'>{loggedInUser}</button>
+      <FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" />
     </div>
   )
 }
