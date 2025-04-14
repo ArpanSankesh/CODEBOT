531440d27a231ef8f14b08b0917c68c8bc3e1ffd
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 21:50:01 +0530

    frontEnd for LOGIN/SIGNUP Page

--- FRONTEND/src/pages/Signup.jsx
+++ FRONTEND/src/pages/Signup.jsx
@@ -1,25 +1,66 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
 
-const Signup = () => {
+
+const Signup = () => { 
+    const [singInInfo, setSingInInfo] = React.useState({
+        name: "",
+        email: "",
+        password: ""
+    })
+
+    const handelChange = (e) => {
+        const {name, value} = e.target;
+        console.log(name, value);
+        const copyLoginInfo = {...singInInfo};
+        copyLoginInfo[name] = value;
+        setSingInInfo(copyLoginInfo);
+    }
+    
+    const handelSubmit = (e) => {
+        e.preventDefault()
+    }
+    
     return (
-        <div className='w-full h-screen flex justify-center items-center flex-col'>
-            <h1 className='text-3xl'>Don't Have an Account?</h1>
-            {/* <p>Create one..</p> */}
-            <form action="POST" className='flex flex-col items-center justify-center gap-4 w-1/3 h-1/2 bg-amber-300 px-10 rounded'>
-                <div className='flex flex-col w-[90%]'>
-                    <label htmlFor="name" className='text-sm text-[#303030]'>Name</label>
-                    <input className='py-1 px-2 text-lg border border-[#0099FF] rounded-md outline-none mt-2 ' type="text" name='name' autoFocus placeholder="Enter your name.." />
-                </div>
-                <div className='flex flex-col w-[90%]'>            
-                    <label htmlFor="email" className='text-sm text-[#303030]'>Email</label>
-                    <input className='py-1 px-2 text-lg border border-[#0099FF] rounded-md outline-none mt-2 ' type="email" name='email' placeholder="Enter your email.." />
+        <div className='w-full h-screen flex justify-center items-center flex-col px-4'>
+            <form onSubmit={handelSubmit}
+                action="POST"
+                className='flex flex-col items-center justify-center gap-8 w-full sm:w-3/4 md:w-2/3 lg:w-1/3 bg-[#f9f9f9] px-6 sm:px-10 py-10 rounded-lg shadow-lg '
+            >
+                <div className='flex flex-col items-center justify-center '>
+                <h1 className='text-xl sm:text-2xl md:text-2xl lg:text-3xl font-bold'>Don't Have an Account?</h1>
+                    <p className='text-gray-500 font-semibold'>Create one..</p>
                 </div>
                 <div className='flex flex-col w-[90%]'>
-                    <label htmlFor="password" className='text-sm text-[#303030]'>Password</label>
-                    <input className='py-1 px-2 text-lg border border-[#0099FF] rounded-md outline-none mt-2 ' type="password" name='pass' placeholder="Enter your password.." />
+                    <input
+                    onChange={handelChange}
+                        className='bg-white py-2 px-2 text-lg border focus:ring-2 focus:ring-[#0099FF] rounded-md outline-none mt-2'
+                        type="text"
+                        name='name'
+                        autoFocus
+                        placeholder="Name.." />
                 </div>
-                <button className='w-1/3 text text-white bg-[#0099FF]' type="submit">Signup</button>
+                <div className='flex flex-col w-[90%]'>
+                    <input
+                    onChange={handelChange}
+                        className='bg-white py-2 px-2 text-lg border focus:ring-2 focus:ring-[#0099FF] rounded-md outline-none mt-2'
+                        type="email"
+                        name='email'
+                        placeholder="Email.." />
+                </div>
+                <div className='flex flex-col w-[90%]'>
+                    <input
+                    onChange={handelChange}
+                        className='bg-white py-2 px-2 text-lg border focus:ring-2 focus:ring-[#0099FF]  rounded-md outline-none mt-2'
+                        type="password"
+                        name='pass'
+                        placeholder="Password.." />
+                </div>
+                <button className='w-[90%] text-2xl p-2 rounded-md text-white bg-[#0099FF] cursor-pointer font-semibold ' type="submit">Sign Up</button>
+                <p className='text-gray-500 font-semibold'>Already have an Account? <Link to="/login" className='text-[#0099FF]'>login</Link></p>
             </form>
+            <ToastContainer/>
         </div>
     )
 }
