fd95c860adce2f975b56bac0b6006bfd2a5d0f11
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 23:31:39 +0530

    frontEnd for LOGIN/SIGNUP Page

--- FRONTEND/src/Components/Header.jsx
+++ FRONTEND/src/Components/Header.jsx
@@ -1,19 +1,35 @@
 import React, { useEffect } from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useNavigate } from 'react-router-dom'
+import { handelSuccess } from '../utils/Utils';
+import { ToastContainer } from 'react-toastify'; 
 
 const Header = () => {
 
   const [loggedInUser, setLoginUser] = React.useState("")
-
+  const nav = useNavigate();
   useEffect(() => {
     setLoginUser(localStorage.getItem("LoggedInUser"))
   }, [])
 
+  const handelLogout = (e) => {
+    localStorage.removeItem("token")
+    localStorage.removeItem("loggedInUser")
+    handelSuccess("Logout Successfully")
+    setTimeout(() => {
+      nav('/login')
+    }, 1000);
+  }
+
   return (
-    <div className='fixed top-0 left-0 z-10 w-full h-[10vh] bg-[#212121] px-10 flex items-center justify-between text-white text-lg'>
+    <div >
+      <div className='fixed top-0 left-0 z-10 w-full h-[10vh] bg-[#212121] px-10 flex items-center justify-between text-white text-lg'>
       <h1 className=''>CodeLens</h1>
-      <button className='bg-transparent border-[0.5px] border-gray-500 px-6 text-lg rounded-xl'>{loggedInUser}</button>
-      <FontAwesomeIcon icon="fa-solid fa-ellipsis-vertical" />
+      <div className='text-lg'>
+      <button className='px-6'>{loggedInUser}</button>
+      <button onClick={handelLogout} className='text-md px-3 py-1  rounded-lg text-white bg-[#0099FF] cursor-pointer font-semibold' >Logout</button>
+      </div>
+      </div>
+      <ToastContainer />
     </div>
   )
 }
