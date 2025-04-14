078550d5def469ce837d2e947252874d297ff09f
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 14:55:46 +0530

    user Auth

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 
+const PORT = process.env.PORT || 8080
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
 })
