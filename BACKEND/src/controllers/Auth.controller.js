881de8f36d44cd5edddeaff1279329b7c5060196
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 18:48:43 +0530

    Auth Validation

--- BACKEND/src/controllers/Auth.controller.js
+++ BACKEND/src/controllers/Auth.controller.js
@@ -1,6 +1,22 @@
+const bcrypt = require("bcryptjs");
+const UserModel = require("../models/User");
 
 
-const signup = (req, res) => {
+const signup = async (req, res) => {
+    try {
+        const {name , email, password} = req.body;
+        const user = await UserModel.findOne({email});
+        if (user) {
+            return res.status(400).json({ message: "User already exists" , success: false});
+        }
+        const UserModel = new UserModel({ name, email, password});
+        UserModel.password = await bcrypt.hash(password, 10);
+        await UserModel.save();
+
+
+    } catch (err) {
+        
+    }
 
 }
 
--- BACKEND/src/routes/auth.routes.js
+++ BACKEND/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const { signupValidation, loginValidation } = require("../controllers/middlewares/auth.middleware");
+const { signup } = require("../controllers/Auth.controller")
 const router = express.Router();
 
 router.post('/login', loginValidation,)
-router.post('/signup', signupValidation,)
+router.post('/signup', signupValidation,signup)
 
 module.exports = router;
