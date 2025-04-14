1dcb9a5ffe9d2a1daa1ef5209c64fbd20826f8e8
ArpanSankesh <arpansankesh830@gmail.com>
2025-04-14 20:07:29 +0530

    frontEnd for LOGIN/SIGNUP Page

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import Hero from './Components/hero'
-import Header from './Components/Header'
+import { Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 
 const App = () => {
   return (
-    <>
-    <Header/>
-    <Hero/>
-    </>
+    <div>hello
+
+    <Routes>
+      <Route path="/home" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+    </Routes>
+    </div>
   )
 }
 
